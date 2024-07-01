b2963b8ece3e703b7afd2cbda0579100c1eaf8ea
nelg62 <glen.harding.nz@gmail.com>
2024-07-01 20:08:39 +1200

    adding new environnment variables

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(
+    `Hello World! this is an evironmentvariable: ${process.env.HELLO_WORLD}  this is an environemtn secret: ${process.env.API_SECRET}`
+  );
 });
 
 app.listen(port, () => {
